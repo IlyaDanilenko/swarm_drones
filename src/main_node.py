1bb8437702590851cdaebe51b176428e43c0766c
IlyaDanilenko <ilya@eadsoft.com>
2021-02-13 08:18:24 +0000

    main update

--- src/main_node.py
+++ src/main_node.py
@@ -5,6 +5,7 @@ import rospy
 from rospy import Subscriber, Publisher 
 from gs_flight import FlightController, CallbackEvent
 from gs_board import BoardManager
+from gs_sensors import SensorManager
 from time import sleep
 from rospy import ServiceProxy
 from swarm_drones.srv import TakeScreen, TakeScreenResponse
@@ -12,72 +13,104 @@ from multimaster.srv import DiscoveryMasters, DiscoveryMastersResponse, Discover
 from gs_interfaces.msg import SimpleBatteryState
 from std_msgs.msgs import Int32, Int16MultiArray
 
+rospy.init_node("main_node")
+sensor_manager = SensorManager()
+_, charge = sensor_manager.power()
+
 f_vac=0 # флаг сделанной фотографии
 num_photos=0 # кол-во сделанных фото
+f_reached=0 # флаг достижения точки при возврате
+f_arr=0
 rospy.init_node("main_node")
 
 coordinates = []# координаты точек
 
-coord_0=coordinates[1]
-h=coord_0[3]
  
 run = True
 position_number = 0
 
+to_home = False
 
-def handle_came(event):
-    event=1
-def gotohome():
+def goToHome():
+    global to_home
+    global h
+    to_home = True
     ap.goToLocalPoint(0, 0, h)
-    ap.landing()
+
 def callback(event):#полёт по точкам
     global ap
     global run
     global coordinates
     global position_number
     global proxy_cam
+    global to_home
+    global num_photos
 
     event = event.data
-    if event == CallbackEvent.ENGINES_STARTED:
-        rospy.loginfo("engine started")
-        ap.takeoff()
-    elif event == CallbackEvent.TAKEOFF_COMPLETE:
-        rospy.loginfo("takeoff complite")
-        position_number = 0
-        ap.goToLocalPoint(coordinates[position_number][0], coordinates[position_number][1], coordinates[position_number][2])
-    elif event == CallbackEvent.POINT_REACHED:
-        rospy.loginfo("point {} reached".format(position_number))
-        f_vac=proxy_cam()
-        if f_vac.stait_img == 1:
-            num_photos=position_number
+    if charge<=10.5:
+        goToHome()
+    else:
+        if event == CallbackEvent.ENGINES_STARTED:
+            rospy.loginfo("engine started")
+            ap.takeoff()
+        if(repeat==0):
+            if event == CallbackEvent.TAKEOFF_COMPLETE:
+                rospy.loginfo("takeoff complite")
+                position_number = 0
+                ap.goToLocalPoint(coordinates[position_number][0], coordinates[position_number][1], coordinates[position_number][2])
+            if event == CallbackEvent.POINT_REACHED:
+                if to_home:
+                    ap.landing()
+                else:
+                    rospy.loginfo("point {} reached".format(position_number))
+                    position_number += 1
+                    if position_number < len(coordinates):
+                        ap.goToLocalPoint(coordinates[position_number][0], coordinates[position_number][1], coordinates[position_number][2])
+            elif event == CallbackEvent.COPTER_LANDED:
+                rospy.loginfo("finish programm")
+                run = False
         else:
-            gotohome()  
-        position_number += 1
-        if position_number < len(coordinates):
-            ap.goToLocalPoint(coordinates[position_number][0], coordinates[position_number][1], coordinates[position_number][2])
-        else:
-            gotohome()
-    elif event == CallbackEvent.COPTER_LANDED:
-        rospy.loginfo("finish programm")
-        run = False
+            if event == CallbackEvent.TAKEOFF_COMPLETE:
+                rospy.loginfo("takeoff complite")
+                ap.updateYaw(0)
+                position_number = 0
+                ap.goToLocalPoint(coordinates[position_number][0], coordinates[position_number][1], coordinates[position_number][2])
+            if event == CallbackEvent.POINT_REACHED:
+                if to_home:
+                    ap.landing()
+                else:
+                    rospy.loginfo("point {} reached".format(position_number))
+                    f_vac=proxy_cam()
+                    if f_vac.stait_img == 1:
+                        num_photos=position_number
+                        position_number += 1
+                    else:
+                        goToHome()
+                        rospy.logwarn("Camera-truble")
+                    if position_number < len(coordinates):
+                        ap.goToLocalPoint(coordinates[position_number][0], coordinates[position_number][1], coordinates[position_number][2])
+                    else:
+                        goToHome()
+            elif event == CallbackEvent.COPTER_LANDED:
+                rospy.loginfo("finish programm")
+                run = False
 
 def callback_point(data):
+    global h
+    global coordinates
     coordinates=data
-    coord_0=coordinates[1]
-    h=coord_0[3]
-def callback_bat(data):
-    global bat
-    bat=data
-    if bat<10.5:
-        gotohome()
+    h=coordinates[1][3]
+def callback_state(data):
+    global repeat
+    repeat = data
 board = BoardManager()
 ap = FlightController(callback)
 once = False
 
 pub_photos = Publisher("white/work_completed", Int32, queue_size=10)
-sub_point=Subscriber("points", Int16MultiArray,callback_point)
-sub_bat=Subscriber("geoscan/battery_state", SimpleBatteryState, callback_bat)
-proxy=ServiceProxy("master_discovery/get_loggers", DiscoveryMasters, )
+sub_point=Subscriber("white/points", Int16MultiArray,callback_point)
+sub_stateRep = Subscriber("swarm_drones/state_repeater", Int32, callback_state)
+#proxy=ServiceProxy("master_discovery/get_loggers", DiscoveryMasters, )
 proxy_cam=ServiceProxy("swarm/take_photos", TakeScreen,)
 
 while not rospy.is_shutdown():
@@ -85,5 +118,5 @@ while not rospy.is_shutdown():
         rospy.loginfo("start programm")
         ap.preflight()
         once = True
-    master = DiscoveryMastersRequest()
-    pub_photos.publish(num_photos/len(coordinates)) #публикуем процент выполненной работы
+    #master = DiscoveryMastersRequest()
+    pub_photos.publish(num_photos/len(coordinates)*100) #публикуем процент выполненной работы
--- src/repeater_node.py
+++ src/repeater_node.py
@@ -3,24 +3,25 @@
 
 import subprocess
 import rospy
-from rospy import ServiceProxy
+from rospy import Publisher, Subscriber
+from rospy import Service
 from swarm_drones.srv import Repeater, RepeaterResponse, RepeaterRequest
-
+from gs_interfaces.msg import SimpleBatteryState
+from std_msgs.msgs import Int32 
 
 rospy.init_node("repeater_node")
 
-networksNames = ["Geoscan-Orange", "Geoscan-White", "Geoscan-Blue"]
+networksNames = ["Geoscan-White", "Geoscan-Blue", "Geoscan-Orange"]
 # subprocess.call("echo connect", shell=True)
 
 def connectToWifi(networkId):
     global networksNames
     print("connecting to ", networkId)
-    subprocess.Popen(["sh","bashscrpt/enableNetworkManager.sh"],stdout=subprocess.PIPE).communicate()
+    subprocess.Popen(["sh","/home/ubuntu/geoscan_ws/src/swarm_drones/bash/enableNetworkManager.sh"],stdout=subprocess.PIPE).communicate()
     subprocess.Popen("nmcli device wifi connect \"{}\" password geoscan123 name \"{}\"".format(networksNames[networkId],networksNames[networkId]), shell=True).communicate()
 
-
 def enableHotspot():
-    subprocess.Popen("sh bashscrpt/enableNetworkManagerHotspot.sh", stdout=subprocess.PIPE).communicate()
+    subprocess.Popen("sh /home/ubuntu/geoscan_ws/src/swarm_drones/bash/enableNetworkManagerHotspot.sh", stdout=subprocess.PIPE).communicate()
 
 def wifiConnect(req):
     if req.command == 1:
@@ -30,5 +31,22 @@ def wifiConnect(req):
     return 1
     # return 192 when complited
 
+"""def callback_req(data):
+    global com
+    com = data"""
 
-proxy = Service("Repeater", Repeater, wifiConnect)
+def callback_bat(data):
+    global power
+    power = data
+
+sub_bat = Subscriber("geoscan/battery_state", SimpleBatteryState, callback_bat)
+#sub_stateRep = Subscriber("swarm_drones/state_repeater", Int32, callback_req)
+pub_stateRep = Publisher("swarm_drones/state_repeater", Int32, queue_size=4)
+proxy = Service("swarm_drones/to_repeater", Repeater, wifiConnect)
+
+
+while not rospy.is_shutdown():
+    if power < 10.5:
+        pub_stateRep.publish(-1)
+    pub_stateRep.publish(0)
+    pass
